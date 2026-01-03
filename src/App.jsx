a3b10c1b87bb8f4c90ac437f23c5f74289eec79b
musaib-014 <musaibahmad1029@gmail.com>
2026-01-03 20:44:46 +0530

    Set up routing and users page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Users from "./pages/Users";
 function App() {
   return (
     <div>
       <h1>User Management</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/users" />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Users.jsx
@@ -0,0 +1,10 @@
+function Users() {
+  return (
+    <div>
+      <h2>Users</h2>
+      <p>Loading Users...</p>
+    </div>
+  );
+}
+
+export default Users;
