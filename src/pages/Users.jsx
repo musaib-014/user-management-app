e22fc6d46fae2d6438fb18585f8a1b731f700ba5
musaib-014 <musaibahmad1029@gmail.com>
2026-01-03 21:53:46 +0530

    Fetch and display users with loading and Error status

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to Fetch Users");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading User....</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div>
       <h2>Users</h2>
-      <p>Loading Users...</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
