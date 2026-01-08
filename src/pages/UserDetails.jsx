11bc0bb3176595a5c4a76e28a491fcce95b8b9ff
musaib-014 <musaibahmad1029@gmail.com>
2026-01-08 22:52:23 +0530

    Fetch and display single user details

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,11 +1,66 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 function UserDetails() {
   const { id } = useParams();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to Fetch User");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) return <p>Loading user details...</p>;
+  if (error)
+    return (
+      <p>
+        Error: {error} <br />
+        <Link to="/users">⬅ Back to Users</Link>
+      </p>
+    );
+  if (!user)
+    return (
+      <p>
+        User not found <br /> <Link to="/users">⬅ Back to Users</Link>
+      </p>
+    );
+
+  console.log(user);
+
   return (
     <div>
-      <h2>User Details :</h2>
-      <p>User ID: {id}</p>
+      <h2>{user.name}</h2>
+      <p>
+        <strong>Email:</strong> {user.email}
+      </p>
+      <p>
+        <strong>Phone:</strong> {user.phone}
+      </p>
+      <p>
+        <strong>Website:</strong> {user.website}
+      </p>
+
+      <p>
+        <strong>Company:</strong> {user.company?.name}
+      </p>
+
+      <Link to="/users">⬅ Back to Users</Link>
     </div>
   );
 }
